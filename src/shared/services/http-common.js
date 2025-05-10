f758a2f2710ed271baf6df0e7c750b8f5104d310
Alexander Jair Castillo <142842509+U202211390@users.noreply.github.com>
2025-05-10 18:36:02 -0500

    fix: fix link api

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {authenticationInterceptor} from "@/domains/IAM/services/authentication.interceptor.js";
 
 const http = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     }
